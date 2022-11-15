94ede08a152bc5d3741f8335760f4d2d41741a39
Andres Hernandez Parra <andres_sim97@hotmail.com>
2022-11-15 09:55:30 +0100

    tema 4 - Hoja 8 - Ejercicio 1

---
+++ 03-FuncionesyArrays/Hoja8/funciones.js
@@ -0,0 +1,34 @@
+export function comprobarCalentamientoGlobal(arr){
+    let aux;
+    for(let tempAnual of arr){
+        if(aux!=undefined){
+            if(aux<tempAnual){
+                aux=tempAnual;
+            }
+            else
+                return false;
+        }
+        else
+            aux=tempAnual;
+    }
+    return true;
+}
+export function calcularMediaAnual(arr){
+    let suma=0;
+    let contador=0;
+    let temperaturasAnuales=[];
+    for(let temperatura of arr){
+        for(let valor of temperatura){
+            suma+=valor;
+        }
+        
+        temperaturasAnuales[contador]=suma/12;
+        temperaturasAnuales[contador]*=100;
+        temperaturasAnuales[contador]=parseInt(temperaturasAnuales[contador]);
+        temperaturasAnuales[contador]/=100;
+        suma=0;
+        
+        contador++;
+    }
+    return temperaturasAnuales;
+}
