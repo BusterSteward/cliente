b945152e071ccbc21b9e2b68edf9355056a9c312
Andres Hernandez Parra <andres_sim97@hotmail.com>
2022-11-29 12:16:32 +0100

    hoja 8 terminada

---
+++ 04-Objetos_Excepciones_Clases/ejercicios/hoja8/ejercicio6.js
@@ -0,0 +1,27 @@
+﻿//6. Dado el siguiente fragmento de código, describe qué ocurre durante su ejecución: 
+
+var pokemon = {
+    firstname: 'Pika',
+    lastname: 'Chu ',
+    getPokeName: function() {
+    var fullname = this.firstname + ' ' + this.lastname;
+    return fullname;
+    }
+   };//creamos un objeto literal llamado pokemon, que tiene 2 propiedades y una funcion
+
+   
+   var pokemonName = function(snack, hobby) {
+    console.log(this.getPokeName() + ' loves ' + snack + ' and ' +
+   hobby);
+   };//creamos una funcion que muestra una cadena por consola, utilizando el objeto this
+   
+   //llamamos a la funcion usando call para cambiar la referencia al this por el objeto que le pasamos
+   //por parámetro, de forma que el que ejecuta la funcion pokemonName, es el objeto pokemon
+   //por lo tanto, dentro de la función, el objeto "this" es el objeto pokemon
+   pokemonName.call(pokemon,'sushi', 'algorithms');
+   //devuelve por consola: "Pikachu loves sushi and algorithms"
+
+   //hacemos lo mismo con apply, la diferencia es que esta función no utiliza un array de tipo rest
+   //por lo que tenemos que pasar el array completo como segundo parámetro
+   pokemonName.apply(pokemon,['sushi', 'algorithms']); 
+   //devuelve por consola: "Pikachu loves sushi and algorithms"
---
+++ 04-Objetos_Excepciones_Clases/ejercicios/hoja8/ejercicio7.js
@@ -0,0 +1,22 @@
+﻿//7. Dado el siguiente fragmento de código, describe qué ocurre durante su ejecución:
+
+
+ var pokemon = {
+ firstname: 'Pika',
+ lastname: 'Chu ',
+ getPokeName: function() {
+ var fullname = this.firstname + ' ' + this.lastname;
+ return fullname;
+ }
+};//creamos un objeto literal llamado pokemon, con 2 propiedades y una funcion
+
+var pokemonName = function() {
+ console.log(this.getPokeName() + 'I choose you!');
+};//creamos una funcion que nos muestra una cadena por consola,utilizando el objeto this
+
+//creamos una funcion nueva que tiene el mismo cuerpo que la funcion pokemonName, pero le cambiamos
+//la referencia al this al objeto que le pasamos como parámetro al bind, por lo que si llamamos a 
+//logPokemon(), el objeto this será pokemon
+var logPokemon = pokemonName.bind(pokemon);
+logPokemon(); 
+//devuelve por consola "Pikachu I choose you!"
