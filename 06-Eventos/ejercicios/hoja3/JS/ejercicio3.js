aa329e7c4fc6252d41ccef2f22833365a2f61665
Andres Hernandez Parra <andres_sim97@hotmail.com>
2023-01-27 14:25:34 +0100

    mucha mierda

---
+++ 06-Eventos/ejercicios/hoja3/JS/ejercicio3.js
@@ -0,0 +1,16 @@
+﻿
+var zone=1;
+const offsetX=157/2;
+const offsetY=181/2;
+copo.addEventListener("drag",function(e){
+    copo.style.position="absolute";
+    copo.style.top=(e.clientY-offsetY)+"px";
+    copo.style.left=(e.clientX-offsetX)+"px";
+});
+contenedor1.addEventListener("drop",function(){
+    console.log("hola")
+});
+contenedor2.addEventListener("drop",function(){
+    console.log("adios")
+});
+
