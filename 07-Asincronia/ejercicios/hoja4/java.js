b02b69fbf1c753fd8511c78976d7d8d53c37c06a
BusterSteward <andres_sim97@hotmail.com>
2023-02-22 20:48:41 +0100

    ejercicio crud acabado

--- 07-Asincronia/ejercicios/hoja4/java.js
+++ 07-Asincronia/ejercicios/hoja4/java.js
@@ -1,5 +1,5 @@
 ﻿iniciar();
-
+var max;
     function cargarListadoCategorias(){
 
         const url="http://localhost:3000/clasificaciones";
@@ -27,6 +27,7 @@
                 fragmento2.appendChild(li);
             });
             
+            
             clasi.appendChild(fragmento1);
             listaCategorias.appendChild(fragmento2);
         })
@@ -44,7 +45,9 @@
         })
         .then(function(datos){
             
+            console.log(datos);
             tabla.innerHTML="<tr><th>Nombre</th><th>Acciones</th></tr>";
+            max=Math.max(...datos.map(obj => obj.id));
             datos.forEach(element => {
                 tr=document.createElement("tr");
                 td=document.createElement("td");
@@ -54,20 +57,20 @@
                 let id=element.id;
                 button=document.createElement("button");
                 button.appendChild(document.createTextNode("Ver más"));
-                button.addEventListener("click",function(id){
+                button.addEventListener("click",function(){
                     leer(id);
                 });
                 td.appendChild(button);
                 button=document.createElement("button");
                 button.appendChild(document.createTextNode("Modificar"));
-                button.addEventListener("click",function(id){
-                    modificar(id);
+                button.addEventListener("click",function(){
+                    mostrarFormulario(id);
                 });
                 td.appendChild(button);
                 button=document.createElement("button");
                 button.appendChild(document.createTextNode("Borrar"));
-                button.addEventListener("click",function(id){
-                    borrar(id);
+                button.addEventListener("click",function(){
+                    borrar(id,this);
                 });
                 td.appendChild(button);
                 tr.appendChild(td);
@@ -82,17 +85,118 @@
         cargarListadoPeliculas(null);
         cargarListadoCategorias();
     }
-    function crear(form){
-        console.log("creando");
+    
+    formulario.addEventListener("submit",crear);
+    function crear(e){
+        //console.log(e.target.nombre.value);
+        
+        
+        let url="http://localhost:3000/peliculas";
+        let obj={
+            id:max+1,
+            nombre:e.target.nombre.value,
+            director:e.target.director.value,
+            clasificacion:e.target.clasi.value
+        }
+        fetch(url,{
+            method:"POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body:JSON.stringify(obj)
+        })
+        .then(resolve=>console.log(resolve.status))
+        
+        .then(data=>{
+            console.log("el registro ha sido creado",data);
+            cargarListadoPeliculas(null);
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+        
     }
     function leer(id){
-        console.log("leyendo");
+        const url="http://localhost:3000/peliculas/"+id;
+        fetch(url)
+        .then(resolve=>resolve.json())
+        .then(data=>{
+            let html=
+            `<h2>Datos de la pelicula</h2>
+            <p><span>Identificador: </span><span>${data.id}</span></p>
+            <p><span>Nombre: </span><span>${data.nombre}</span></p>
+            <p><span>Director: </span><span>${data.director}</span></p>
+            <p><span>Categoria: </span><span>${data.clasificacion}</span></p>
+            <button id="volver">Atras</button>`;
+            juegos.innerHTML=html;
+            document.getElementById("volver").addEventListener("click",function(){
+                juegos.innerHTML="<h2>Listado de juegos</h2><table id='tabla'></table>"
+                cargarListadoPeliculas(null);
+            })
+        })
 
     }
-    function borrar(id){
-        console.log("borrando");
+    function borrar(id,boton){
+        console.log(id);
+        let row=boton.parentNode.parentNode;
+        tabla.removeChild(row);
+
+        let url="http://localhost:3000/peliculas/"+id;
+        fetch(url,{
+            method:"DELETE"
+        })
+        .then(resolve=>{
+            console.log("pelicula eliminada");
+        })
 
     }
-    function modificar(id){
-        console.log("modificando");
+    function mostrarFormulario(id){
+        
+        fetch("http://localhost:3000/clasificaciones")
+        .then(resolve=>resolve.json())
+        .then(datos=>{
+            
+            let html=
+            `<h2>Formulario de modificación</h2>
+            <form id="formulario2" action="" onsubmit="return false;">
+            <p><label for="nombreM">Nombre: </label><input id="nombreM" name="nombreM" type="text"/></p>
+            <p><label for="directorM">Director: </label><input id="directorM" name="directorM" type="text"/></p>
+            <p><label for="clasiM">Clasificación: </label><select id="clasiM" name="clasiM">`
+            datos.forEach(cat=>{
+                html+="<option value='"+cat.nombre+"'>"+cat.nombre+"</option>";
+            })
+            html+=`</select></p>
+            <p><input type="reset" value="limpiar"/><input type="submit" value="modificar"/></p>
+            </form>
+            <button id="volver">Cancelar</button>`;
+            juegos.innerHTML=html;
+            formulario2.addEventListener("submit",function(){
+                modificar(id,this)
+            });
+            document.getElementById("volver").addEventListener("click",function(){
+                juegos.innerHTML="<h2>Listado de juegos</h2><table id='tabla'></table>"
+                cargarListadoPeliculas(null);
+            });
+        })
+
+    }
+    function modificar(id,form){
+        const url="http://localhost:3000/peliculas/"+id;
+        let obj={
+            id:id,
+            nombre:form.nombreM.value,
+            director:form.directorM.value,
+            clasificacion:form.clasiM.value
+        }
+        fetch(url,{
+            method:"PUT",
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(obj)
+        })
+        .then(resolve=>{
+            juegos.innerHTML="<h2>Listado de juegos</h2><table id='tabla'></table>"
+                cargarListadoPeliculas(null);
+        })
     }
